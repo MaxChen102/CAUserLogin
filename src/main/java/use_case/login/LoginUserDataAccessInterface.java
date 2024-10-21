bf317d45fcdce64690b79a8cfba2f48e844b9884
MaxChen102 <maxx.chen@mail.utoronto.ca>
2024-10-20 23:32:13 -0400

    "Task 2.2"

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,5 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets current username.
+     * @param name new username
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Return current username.
+     * @return current username
+     */
+    String getCurrentUser();
 }
